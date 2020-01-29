5538adb91bade2ffd11d1ed89941603c856e89fb
jianglei <1021124094@qq.com>
2020-01-29 15:11:41 +0800

    添加icon和iconPosition属性

--- src/button.vue
+++ src/button.vue
@@ -1,32 +1,62 @@
-<template>
-    <button class="g-button">button</button>
+<template lang="pug">
+    button(class="g-button", :class="{[`icon-${iconPosition}`]: true}")
+        svg(v-if="icon" class="icon")
+            use(:xlink:href="`#icon-${icon}`")
+        .content
+            slot
 </template>
 
 <script>
-    export default {}
-
+export default {
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator: function (value) {
+        return value === 'left' || value === 'right'
+      }
+    }
+  }
+}
 </script>
 
-<style scoped lang="stylus">
-    .g-button {
-        font-size: var(--font-size);
-        height: var(--button-height);
-        padding: 0 1em;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-bg);
+<style lang="stylus" scoped>
+    .g-button
+        font-size var(--font-size)
+        height var(--button-height)
+        padding 0 1em
+        border-radius var(--border-radius)
+        border 1px solid var(--border-color)
+        background var(--button-bg)
+        display flex
+        justify-content center
+        align-items center
+        vertical-align middle
 
-        &:hover {
-            border-color: var(--border-color-hover);
-        }
+        &:hover
+            border-color var(--border-color-hover)
 
-        &:active {
-            background: var(--button-action-bg);
-        }
+        &:active
+            background var(--button-action-bg)
 
-        &:focus {
-            outline: none;
-        }
-    }
+        &:focus
+            outline none
 
+        > .icon
+            width 1em
+            height 1em
+            order 1
+            margin-right .5em
+
+        > .content
+            order 2
+
+        &.icon-right
+            > .content
+                order 1
+
+            > .icon
+                order 2
+                margin 0 0 0 .5em
 </style>
