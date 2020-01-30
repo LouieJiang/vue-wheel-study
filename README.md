f403d83dfda8021a1fb82b254dc73a406e7d1cdd
jianglei <1021124094@qq.com>
2020-01-31 00:21:20 +0800

    Karma + Mocha做单元测试,以及使用travis CI 持续集成测试

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 ## 安装
 使用本框架请开启border-box
+
+
+Windows 用户运行 npm run dev-test 时会出现 BUG，解决办法是：
+将 dev-test 对应的命令 parcel watch test/* --no-cache & karma start 分别运行，运行方式如下
+新开一个 Git Bash 窗口运行 npx parcel watch test/* --no-cache
+再开一个 Git Bash 窗口运行 npx karma start
--- src/app.js
+++ src/app.js
@@ -2,86 +2,103 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
-
-Vue.component('g-button',Button);
-Vue.component('g-icon',Icon);
-Vue.component('g-button-group',ButtonGroup);
-
-new Vue({
-    el:"#app",
-    data:{
-        loading1: false
-    }
-});
-
 //单元测试
 import chai from 'chai'
-const expect = chai.expect
-{
-    const Constructor = Vue.extend(Button);
-    const vm = new Constructor({
-        propsData:{
-            icon:"settings",
+import spise from 'chai-spies'
 
-        }
-    });
+Vue.component('g-button', Button)
+Vue.component('g-icon', Icon)
+Vue.component('g-button-group', ButtonGroup)
+
+new Vue({
+  el: '#app',
+  data: {
+    loading1: false
+  }
+})
+
+chai.use(spise)
+const expect = chai.expect
+
+try {
+  console.log('------------开始测试---------------')
+  {
+    //测试按钮含有 icon
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+
+      }
+    })
     vm.$mount()
     let useElement = vm.$el.querySelector('use')
-    //console.log(useElement);
+    console.log('vm.$el',vm.$el);
+    console.log('useElement',useElement);
     let href = useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#icon-settings');
+    expect(href).to.eq('#icon-settings')
     vm.$el.remove()
     vm.$destroy()
-}
+  }
 
-{
-    const Constructor = Vue.extend(Button);
+  {
+    //传入loading为true 显示loading
+    const Constructor = Vue.extend(Button)
     const vm = new Constructor({
-        propsData:{
-            icon:"settings",
-            loading: true
-        }
-    });
+      propsData: {
+        icon: 'settings',
+        loading: true
+      }
+    })
     vm.$mount()
-    let useElement = vm.$el.querySelector('use');
-    let href = useElement.getAttribute('xlink:href');
-    expect(href).to.eq('#icon-loading');
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#icon-loading')
     vm.$el.remove()
     vm.$destroy()
-}
+  }
 
-{
-    const div = document.createElement('div');
-    document.body.appendChild(div);
-    const Constructor = Vue.extend(Button);
+  {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
     const vm = new Constructor({
-        propsData: {
-            icon: 'settings'
-        }
-    });
+      propsData: {
+        icon: 'settings'
+      }
+    })
     vm.$mount(div)
-    let svg = vm.$el.querySelector('svg');
-    console.log('svg',svg)
-    let {order} = window.getComputedStyle(svg)
+    let svg = vm.$el.querySelector('svg')
+    console.log('svg', svg)
+    let { order } = window.getComputedStyle(svg)
     expect(order).to.eq('1')
     vm.$el.remove()
     vm.$destroy()
 
-}
+  }
 
-{
-    const Constructor = Vue.extend(Button);
+  {
+    const Constructor = Vue.extend(Button)
     const vm = new Constructor({
-        propsData: {
-            icon: 'settings'
-        }
-    });
-    vm.$mount()
-    vm.$on('click',function () {
-        console.log(1)
+      propsData: {
+        icon: 'settings'
+      }
     })
+    vm.$mount()
+    let spy = chai.spy(function () {})
+    vm.$on('click', spy)
     let button = vm.$el
     button.click()
+    expect(spy).to.have.been.called()
     vm.$el.remove()
     vm.$destroy()
+  }
+  console.log('------------测试结束---------------')
+
+} catch (error) {
+  window.errors = [error]
+} finally {
+  window.errors && window.errors.forEach((error) => {
+    console.error(error.message)
+  })
 }
