d5fa4cd534deb213ce11730e24790d41c199bffd
jianglei <1021124094@qq.com>
2020-02-04 20:56:11 +0800

    row 和 col 雏形完成

---
+++ src/col.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+    .col(:class="[`col-${span}`]")
+
+</template>
+
+<script>
+export default {
+  name: 'col',
+  props:{
+    span:{type:[Number,String]}
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+    .col
+        height 100px
+        background grey
+        border 1px solid red
+        width 50%
+
+        for n in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+
+            &.col-{n}
+                width (n / 24) * 100%
+</style>
---
+++ src/row.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+    .row
+        slot
+</template>
+
+<script>
+export default {
+  name: 'row'
+}
+</script>
+
+<style scoped lang="stylus">
+    .row
+        display flex
+        wight 100%
+
+</style>
